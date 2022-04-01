f44e65e66394cd50f209fe5bcd0f4719c66075f8
sbx_user1051 <sbx_user1051@169.254.245.197>
2022-04-01 13:03:58 +0000

    Fri Apr 01 13:03:58 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,11 +1,26 @@
 package befaster.solutions.CHK;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CheckoutSolution {
+    private final List<MarketItem> marketItems = new ArrayList<>();
+    private final Map<MarketItem, MarketSpecialOffer> specialOffers = new HashMap<>();
+    
     public Integer checkout(String skus) {
         
     }
     
+    private void generateItems(String skus) {
+        String[] itemRows = skus.split("\n");
+    }
     
+    private void generateItem(String itemSku) {
+        
+    }
 }
+
 
 
