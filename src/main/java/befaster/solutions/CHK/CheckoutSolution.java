7a0f18b28850cbff349fbc64be417624f881ca67
sbx_user1051 <sbx_user1051@169.254.245.197>
2022-04-01 13:22:14 +0000

    Fri Apr 01 13:22:14 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CheckoutSolution {
-    private static final Map<String, MarketItem> itemForTags = new HashMap<String, MarketItem>(){{
+    private static final Map<String, MarketItem> ITEMS_FOR_TAGS = new HashMap<String, MarketItem>(){{
         put("A", new MarketItem("A", 50, new MarketSpecialOffer(3, 130)));
         put("B", new MarketItem("B", 30, new MarketSpecialOffer(2, 45)));
         put("C", new MarketItem("C", 20, null));
@@ -14,7 +14,30 @@ public class CheckoutSolution {
     private final Market market = new Market();
     
     public Integer checkout(String skus) {
-        return -1;
+        boolean itemsProcessed = processItems(skus);
+        if(!itemsProcessed) {
+            return -1;
+        }
+        
+        // TO DO - price calculation
     }
-
+    
+    private boolean processItems(String skus) {
+        for(int i = 0; i < skus.length(); i++) {
+            if(processItem(skus.charAt(i) + "") == false) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private boolean processItem(String itemTag) {
+        if(ITEMS_FOR_TAGS.containsKey(itemTag) == false) {
+            return false;
+        }
+        
+        
+    }
 }
+
