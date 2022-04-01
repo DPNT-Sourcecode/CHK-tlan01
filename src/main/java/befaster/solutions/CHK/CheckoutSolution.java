f201d4954ea0f1099df58ea504d346554bb3def9
sbx_user1051 <sbx_user1051@169.254.22.93>
2022-04-01 15:53:14 +0000

    Fri Apr 01 15:53:14 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -14,7 +14,35 @@ public class CheckoutSolution {
         put("D", new MarketItem("D", 15, MarketPriceSpecialOffer.emptyOffer()));
         put("E", new MarketItem("E", 40, new MarketItemFreeSpecialOffer(2, "B")));
         put("F", new MarketItem("F", 10, new MarketItemSameFreeSpecialOffer(2)));
+        put("G", new MarketItem("G", 20, MarketPriceSpecialOffer.emptyOffer()));
+        put("H", new MarketItem("H", 10, 
+                new MarketPriceSpecialOffer(5, 45),
+                new MarketPriceSpecialOffer(10, 80)
+        ));
+        put("I", new MarketItem("I", 35, MarketPriceSpecialOffer.emptyOffer()));
+        put("J", new MarketItem("J", 60, MarketPriceSpecialOffer.emptyOffer()));
     }};
+    
+    /**
+| J    | 60    |                        |
+| K    | 80    | 2K for 150             |
+| L    | 90    |                        |
+| M    | 15    |                        |
+| N    | 40    | 3N get one M free      |
+| O    | 10    |                        |
+| P    | 50    | 5P for 200             |
+| Q    | 30    | 3Q for 80              |
+| R    | 50    | 3R get one Q free      |
+| S    | 30    |                        |
+| T    | 20    |                        |
+| U    | 40    | 3U get one U free      |
+| V    | 50    | 2V for 90, 3V for 130  |
+| W    | 20    |                        |
+| X    | 90    |                        |
+| Y    | 10    |                        |
+| Z    | 50    |                        |
++------+-------+------------------------+
+     */
     
     private Market market;
     
@@ -52,3 +80,4 @@ public class CheckoutSolution {
         return market.getMarketValue();
     }
 }
+
